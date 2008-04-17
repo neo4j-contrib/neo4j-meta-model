4dfc1339c63f4d45fce72afe803bcdd5cee4501e
Mattias Persson <mattias@neotechnology.com>
2008-04-17 12:01:35 +0000

    Removed the optimized size cache since it would be harder to implement other collections if you'd have to sync the "size" property.

--- src/main/java/org/neo4j/neometa/structure/MetaStructureInstanceCollection.java
+++ src/main/java/org/neo4j/neometa/structure/MetaStructureInstanceCollection.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import org.neo4j.api.core.Direction;
 import org.neo4j.api.core.Node;
 import org.neo4j.api.core.Relationship;
-import org.neo4j.api.core.Transaction;
 import org.neo4j.util.NeoRelationshipSet;
 
 /**
@@ -42,39 +41,39 @@ public class MetaStructureInstanceCollection extends NeoRelationshipSet<Node>
 		return node;
 	}
 	
-	@Override
-	public boolean add( Node item )
-	{
-		Transaction tx = meta.neo().beginTx();
-		try
-		{
-			boolean result = super.add( item );
-			if ( result )
-			{
-				int count = size() + 1;
-				getUnderlyingNode().setProperty( KEY_COUNT, count );
-			}
-			tx.success();
-			return result;
-		}
-		finally
-		{
-			tx.finish();
-		}
-	}
-	
-	@Override
-	protected void removeItem( Relationship rel )
-	{
-		super.removeItem( rel );
-		int count = size() - 1;
-		getUnderlyingNode().setProperty( KEY_COUNT, count );
-	}
-	
-	@Override
-	public int size()
-	{
-		return ( ( Number ) meta.neoUtil().getProperty(
-			getUnderlyingNode(), KEY_COUNT, 0 ) ).intValue();
-	}
+//	@Override
+//	public boolean add( Node item )
+//	{
+//		Transaction tx = meta.neo().beginTx();
+//		try
+//		{
+//			boolean result = super.add( item );
+//			if ( result )
+//			{
+//				int count = size() + 1;
+//				getUnderlyingNode().setProperty( KEY_COUNT, count );
+//			}
+//			tx.success();
+//			return result;
+//		}
+//		finally
+//		{
+//			tx.finish();
+//		}
+//	}
+//	
+//	@Override
+//	protected void removeItem( Relationship rel )
+//	{
+//		super.removeItem( rel );
+//		int count = size() - 1;
+//		getUnderlyingNode().setProperty( KEY_COUNT, count );
+//	}
+//	
+//	@Override
+//	public int size()
+//	{
+//		return ( ( Number ) meta.neoUtil().getProperty(
+//			getUnderlyingNode(), KEY_COUNT, 0 ) ).intValue();
+//	}
 }
